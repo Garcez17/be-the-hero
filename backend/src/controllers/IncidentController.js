8bc925991488f915f987c67bc2155d2f0d301ce0
Gabriel <ggarcez613@gmail.com>
2020-03-30 13:43:35 -0300

    correction in case update

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -37,7 +37,6 @@ module.exports = {
 
     return res.json({ id });
   },
-
   
   async update(req, res) {
     const { id } = req.params;
@@ -46,19 +45,22 @@ module.exports = {
 
     const incident = await connection('incidents')
       .where('id', id)
-      .select('ong_id', 'title', 'description', 'value')
+      .select('ong_id')
       .first()  
 
     if(incident.ong_id !== ong_id) {
       return res.status(401).json({ error: 'Operation not permitted.' });
     }
 
-    await connection('incidents').update({
-      title: title,
-      description: description,
-      value: value,
-    });
-
+    await connection('incidents')
+      .where('id', id)
+      .select('title', 'description', 'value')
+      .update({
+        title: title,
+        description: description,
+        value: value,
+      });
+      
     return res.status(204).send();
   },
 
